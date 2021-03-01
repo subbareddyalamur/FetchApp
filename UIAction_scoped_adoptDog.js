3c67a26fba29f21853a0cf9f946f1fc34e21b181
Alamuru <subbareddy.alamuru@sabre.com>
2021-03-01 11:44:50 +0530

    create UI action

---
+++ UIAction_scoped_adoptDog.js
@@ -0,0 +1,21 @@
+// client checked
+//show insert unchecked
+//onclick - adoptDog
+//condition - current.status == 'active'
+
+function adoptDog() {
+	var email = prompt('Please enter email id');
+	if(email != '') {
+		var ga = new GlideAjax('fetchUtils');
+		ga.addParam('sysparm_name','createEmailNotification');
+		ga.addParam('sysparm_adoption_center', g_form.getValue('adoption_center'));
+		ga.addParam('sysparm_adopter_email',email);
+		ga.getXML(ajaxProcessor);
+	}	else {
+		alert('The email id you entered is not valid');
+	}
+}
+
+function ajaxProcessor(answer) {
+	alert('Thank you for your request. Someone will be in touch with you shortly.');
+}
